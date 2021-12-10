564804f229fefa945af6aed56120e70e2aff7a24
Danil Proskurin <d.proskurin@anchorfree.com>
2021-12-10 17:56:11 +0700

    Fixed test

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -4,7 +4,8 @@ from pages.base_page import BasePage
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.webdriver.common.by import By
-from settings import Const, Urls
+from settings import Const
+from pages.base_page import LoginPageLocators
 
 
 class LandingPage(BasePage):
@@ -12,6 +13,6 @@ class LandingPage(BasePage):
     def verify_forgot_password_screen_opened(self):
         self.find_and_click_element_by_visible_text(Const.EMAIL)
         self.find_and_click_element_by_visible_text("Logout")
-        self.find_and_click_element_by_visible_text("Sign in")
+        self.driver.find_element_by_css_selector("button[role='button'] > span").click()
         self.find_and_click_element_by_visible_text("Forgot password?")
         return self.element_by_visible_text_is_present("Forgotten password", "div")
