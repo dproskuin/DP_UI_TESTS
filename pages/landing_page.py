20cd3b15c675e8a62107385aaae96bfc9a044a26
Danil Proskurin <d.proskurin@anchorfree.com>
2022-01-10 12:53:18 +0700

    fix documentation tests

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -1,4 +1,6 @@
 """This module contains Landing page interaction methods (Page object)."""
+import time
+
 from pages.base_page import BasePage
 from settings import Const
 
@@ -19,15 +21,18 @@ class LandingPage(BasePage):
 
     def verify_developer_documentation_page_available(self):
         self.find_and_click_element_by_visible_text("Pango Developer Documentation")
-        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        time.sleep(1)
         return self.driver.current_url
 
     def verify_privacy_policy_page_available(self):
         self.find_and_click_element_by_visible_text("Privacy Policy")
-        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        time.sleep(1)
         return self.driver.current_url
 
     def verify_terms_of_service_page_available(self):
         self.find_and_click_element_by_visible_text("Terms of Service")
-        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        time.sleep(1)
         return self.driver.current_url
